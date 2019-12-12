1dc8ca0c33c45a36df21c2c905b1379831ab171a
lufficc <luffy.lcc@gmail.com>
2019-12-12 18:49:40 +0800

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='Vizer',
-    version='0.1.4',
+    version='0.1.5',
     author='lufficc',
     author_email='luffy.lcc@gmail.com',
     packages=['vizer'],
